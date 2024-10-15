35e0544abf9b3831b2987eeb412631ee9dcc9b51
Tiago Castro <tiagocastro044@gmail.com>
2024-10-15 20:34:33 +0100

    aula 0 - Exemplo de criação de dll

---
+++ ApresentacaoDLL/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApresentacaoDLL
+{
+    public class Aluno
+    {
+        #region Atributos
+        private string nome = "Default";
+
+        #endregion
+
+        #region Metodos
+
+        public void SetNomeQualQuerCoisa(string nomeParametro) //André
+        {
+                this.nome = nomeParametro;
+        }
+
+        public string GetNome()
+        {
+            return this.nome; //Default
+        }
+        #endregion
+
+    }
+}
